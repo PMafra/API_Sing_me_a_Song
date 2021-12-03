15d8c95a38cde1629f7549e9b02f0b3cd9bf64e2
PMafra <pmafrarm@gmail.com>
2021-12-03 12:15:17 -0300

    Finish insert recommendation service unit test

---
+++ tests/unit/recommendationService.test.js
@@ -0,0 +1,19 @@
+import * as recommendationRepository from '../../src/repositories/recommendationRepository.js';
+import * as recommendationService from '../../src/services/recommendationService.js';
+
+const sut = recommendationService;
+jest.mock('youtube-validate');
+
+describe('Recommendation service test', () => {
+  it('Should return "addedPoint" for recommendation already existing', async () => {
+    const mockRecommendationBody = {
+      name: 'Falamansa - Xote dos Milagres',
+      link: 'https://www.youtube.com/watch?v=chwyjJbcs1Y',
+    };
+    jest.spyOn(recommendationRepository, 'selectRecommendation').mockImplementationOnce(() => true);
+    jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() => true);
+
+    const result = await sut.insertRecommendation(mockRecommendationBody);
+    expect(result).toEqual('addedPoint');
+  });
+});
