aeb8eab64f1ec0e53f0fffc5969899086182a0b4
PMafra <pmafrarm@gmail.com>
2021-12-04 13:01:28 -0300

    Finish increase recommendation score service unit test

--- tests/unit/recommendationService.test.js
+++ tests/unit/recommendationService.test.js
@@ -1,31 +1,52 @@
 import * as recommendationRepository from '../../src/repositories/recommendationRepository.js';
 import * as recommendationService from '../../src/services/recommendationService.js';
+import NotFoundError from '../../src/errors/notFoundError.js';
 
 const sut = recommendationService;
 jest.mock('youtube-validate');
 
 describe('Recommendation service test', () => {
   it('Should return "addedPoint" for recommendation already existing', async () => {
-    const mockRecommendationBody = {
+    const mockRecommendationObject = {
       name: 'Falamansa - Xote dos Milagres',
       youtubeLink: 'https://www.youtube.com/watch?v=chwyjJbcs1Y',
     };
     jest.spyOn(recommendationRepository, 'selectRecommendation').mockImplementationOnce(() => true);
     jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() => true);
 
-    const result = await sut.insertRecommendation(mockRecommendationBody);
+    const result = await sut.insertRecommendation(mockRecommendationObject);
     expect(result).toEqual('addedPoint');
   });
 
   it('Should return true for new song recommendation', async () => {
-    const mockRecommendationBody = {
+    const mockRecommendationObject = {
       name: 'Falamansa - Xote dos Milagres',
       youtubeLink: 'https://www.youtube.com/watch?v=chwyjJbcs1Y',
     };
     jest.spyOn(recommendationRepository, 'selectRecommendation').mockImplementationOnce(() => false);
     jest.spyOn(recommendationRepository, 'insertRecommendation').mockImplementationOnce(() => true);
 
-    const result = await sut.insertRecommendation(mockRecommendationBody);
+    const result = await sut.insertRecommendation(mockRecommendationObject);
+    expect(result).toEqual(true);
+  });
+
+  it('Should return Not Found Error for not existant id', async () => {
+    const mockUpvoteId = {
+      id: 1,
+    };
+    jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() => false);
+
+    const promise = sut.increaseScore(mockUpvoteId);
+    await expect(promise).rejects.toThrowError(NotFoundError);
+  });
+
+  it('Should return true for increased recommendation score', async () => {
+    const mockUpvoteId = {
+      id: 1,
+    };
+    jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() => true);
+
+    const result = await sut.increaseScore(mockUpvoteId);
     expect(result).toEqual(true);
   });
 });
