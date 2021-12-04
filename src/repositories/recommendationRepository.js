26a1548141cda878931653358d46bff967298301
PMafra <pmafrarm@gmail.com>
2021-12-04 17:51:29 -0300

    Refactor repository - create select constant

--- src/repositories/recommendationRepository.js
+++ src/repositories/recommendationRepository.js
@@ -1,20 +1,55 @@
 /* eslint-disable no-console */
 import connection from '../database/database.js';
 
+const selectString = 'SELECT * FROM "songs"';
+
+const selectAll = async () => {
+  const result = await connection.query(
+    `${selectString};`,
+  );
+  return result.rows;
+};
+
+const selectRandom = async ({ filter }) => {
+  let filterQuery = '';
+
+  if (filter === 70) {
+    filterQuery = ' WHERE score > 10';
+  }
+  if (filter === 30) {
+    filterQuery = ' WHERE score BETWEEN -5 AND 10';
+  }
+
+  const result = await connection.query(
+    `${selectString}${filterQuery} ORDER BY random() LIMIT 1;`,
+  );
+
+  return result.rows[0];
+};
+
+const selectTop = async ({ amount }) => {
+  const result = await connection.query(
+    `${selectString} ORDER BY score DESC LIMIT $1;`,
+    [amount],
+  );
+
+  return result.rows;
+};
+
+const selectRecommendation = async ({ name }) => {
+  const result = await connection.query(
+    `${selectString} WHERE name ILIKE $1;`,
+    [name],
+  );
+  return result.rows[0];
+};
+
 const insertRecommendation = async ({ name, youtubeLink }) => {
   const result = await connection.query(
     'INSERT INTO "songs" (name, "youtubeLink", score) VALUES ($1, $2, $3);',
     [name, youtubeLink, 1],
   );
   return result;
-};
-
-const selectRecommendation = async ({ name }) => {
-  const result = await connection.query(
-    'SELECT * FROM "songs" WHERE name ILIKE $1;',
-    [name],
-  );
-  return result.rows[0];
 };
 
 const updateScore = async ({ id, type }) => {
@@ -39,40 +74,6 @@ const deleteRecommendation = async ({ id }) => {
     [id],
   );
   return result;
-};
-
-const selectAll = async () => {
-  const result = await connection.query(
-    'SELECT * FROM "songs";',
-  );
-  return result.rows;
-};
-
-const selectRandom = async ({ filter }) => {
-  let filterQuery = '';
-
-  if (filter === 70) {
-    filterQuery = ' WHERE score > 10';
-  }
-  if (filter === 30) {
-    filterQuery = ' WHERE score BETWEEN -5 AND 10';
-  }
-
-  const result = await connection.query(
-    `SELECT * FROM "songs"${filterQuery} ORDER BY random() LIMIT 1;`,
-  );
-
-  return result.rows[0];
-};
-
-const selectTop = async ({ amount }) => {
-  console.log('result');
-  const result = await connection.query(
-    'SELECT * FROM "songs" ORDER BY score DESC LIMIT $1;',
-    [amount],
-  );
-
-  return result.rows;
 };
 
 export {
