8afa897cb9be2e64e3c553f07f216a22ff05e1e0
PMafra <pmafrarm@gmail.com>
2021-12-05 11:31:26 -0300

    Finish create mock functions in getTop and insertRecommendations tests

--- tests/unit/recommendationService/insertRecommendation.test.js
+++ tests/unit/recommendationService/insertRecommendation.test.js
@@ -10,19 +10,35 @@ const mockRecommendationBody = recommendationFactory.createRecomendations(
 const mockRecommendation = recommendationFactory.createRecomendations({
   length: 1, score: 1, isBody: false,
 });
+const mockSelect = jest.spyOn(recommendationRepository, 'selectQuery');
+const mockUpdate = jest.spyOn(recommendationRepository, 'updateScore');
+const mockInsert = jest.spyOn(recommendationRepository, 'insertRecommendation');
+
+const mockRecommendationRepository = {
+  selectQuery: {
+    undefined: () => mockSelect.mockImplementationOnce(() => undefined),
+    recommendation: () => mockSelect.mockImplementationOnce(() => mockRecommendation),
+  },
+  updateScore: {
+    true: () => mockUpdate.mockImplementationOnce(() => true),
+  },
+  insertRecommendation: {
+    true: () => mockInsert.mockImplementationOnce(() => true),
+  },
+};
 
 describe('Insert recommendation service tests', () => {
   it('Should return "addedPoint" when recommendation already exists', async () => {
-    jest.spyOn(recommendationRepository, 'selectQuery').mockImplementationOnce(() => mockRecommendation);
-    jest.spyOn(recommendationRepository, 'updateScore').mockImplementationOnce(() => true);
+    mockRecommendationRepository.selectQuery.recommendation();
+    mockRecommendationRepository.updateScore.true();
 
     const result = await sut.insertRecommendation(mockRecommendationBody);
     expect(result).toEqual('addedPoint');
   });
 
   it('Should return true for new song recommendation', async () => {
-    jest.spyOn(recommendationRepository, 'selectQuery').mockImplementationOnce(() => undefined);
-    jest.spyOn(recommendationRepository, 'insertRecommendation').mockImplementationOnce(() => true);
+    mockRecommendationRepository.selectQuery.undefined();
+    mockRecommendationRepository.insertRecommendation.true();
 
     const result = await sut.insertRecommendation(mockRecommendationBody);
     expect(result).toBeTruthy();
